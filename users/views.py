a60bd86cdf62b8e8689abc2a534565e329a250f4
zhoubo <congminghaoxue@outlook.com>
2017-07-07 09:46:26 +0800

    add json response

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 # from django.http import JsonResponse
-# from django.core import serializers
+from django.core import serializers
 # Create your views here.
 # 导入模型中的UserModel表
 from .models import UserModel
@@ -9,13 +9,19 @@ from .models import UserModel
 
 def users(request):
     # 获取所有的用户
-    all_user = UserModel.objects.all()
-    # 把用户信息和前端文件一起发送到浏览器
+    all_user = UserModel.objects.all() #values('name')
 
     """
     todo : response to json
     """
-    # all_user = serializers.serialize('json', all_user, fields=('name','phone'))
-    # return JsonResponse(dict(all_user))
+    # XMLSerializer = serializers.get_serializer("xml")
+    # xml_serializer = XMLSerializer()
+    # xml_serializer.serialize(all_user)
+    # data = xml_serializer.getvalue()
+
+    data = serializers.serialize('json', all_user, fields=('name'))
+
+    # 把用户信息和前端文件一起发送到浏览器
+    return HttpResponse(data)
 
     return render(request, 'users.html', {'all_user': all_user})
