3f49bff0aaf912d5f6e0b9e058a5862518caf0c3
zhoubo <congminghaoxue@outlook.com>
2017-07-10 09:20:24 +0800

    add detect user agent

--- website/urls.py
+++ website/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from users.views import home
 
 urlpatterns = [
+    url(r'^$', home),
     url(r'^admin/', admin.site.urls),
     url(r'^users/',include('users.urls')),
 ]
